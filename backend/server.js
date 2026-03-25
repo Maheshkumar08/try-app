5e0e13099d1f66abeebce6bed1373d07fc0a9916
Maheshkumar08 <maheshkumar00328@example.com>
2026-03-25 22:05:32 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -203,7 +203,7 @@ app.use((err, req, res, next) => {
 });
 
 // ================= SERVER =================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, "0.0.0.0", () => {
   console.log("=================================");
